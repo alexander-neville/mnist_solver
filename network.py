12544cdaa816b2155a53dc1c89cc748419a6d465
Alexander Neville <alexander.neville@icloud.com>
2021-11-25 23:13:19 +0000

    refactor for consistent & concise data handling

--- network.py
+++ network.py
@@ -106,14 +106,7 @@ class Network(object):
             a = sigmoid(np.dot(w, a)+b)
         return a
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta,
-            lmbda = 0.0,
-            evaluation_data=None,
-            monitor_evaluation_cost=False,
-            monitor_evaluation_accuracy=False,
-            monitor_training_cost=False,
-            monitor_training_accuracy=False,
-            early_stopping_n = 0):
+    def SGD(self, training_data, epochs, mini_batch_size, eta, lmbda = 0.0):
         """Train the neural network using mini-batch stochastic gradient
         descent.  The ``training_data`` is a list of tuples ``(x, y)``
         representing the training inputs and the desired outputs.  The
@@ -134,65 +127,20 @@ class Network(object):
 
         """
 
-        # early stopping functionality:
-        best_accuracy=1
-
         training_data = list(training_data)
         n = len(training_data)
 
-        if evaluation_data:
-            evaluation_data = list(evaluation_data)
-            n_data = len(evaluation_data)
-
-        # early stopping functionality:
-        best_accuracy=0
-        no_accuracy_change=0
-
-        evaluation_cost, evaluation_accuracy = [], []
-        training_cost, training_accuracy = [], []
         for j in range(epochs):
             random.shuffle(training_data)
             mini_batches = [
                 training_data[k:k+mini_batch_size]
                 for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
-                self.update_mini_batch(
-                    mini_batch, eta, lmbda, len(training_data))
+                self.update_mini_batch( mini_batch,
+                                        eta, lmbda,
+                                        len(training_data))
 
             print("Epoch %s training complete" % j)
-
-            if monitor_training_cost:
-                cost = self.total_cost(training_data, lmbda)
-                training_cost.append(cost)
-                print("Cost on training data: {}".format(cost))
-            if monitor_training_accuracy:
-                accuracy = self.accuracy(training_data, convert=True)
-                training_accuracy.append(accuracy)
-                print("Accuracy on training data: {} / {}".format(accuracy, n))
-            if monitor_evaluation_cost:
-                cost = self.total_cost(evaluation_data, lmbda, convert=True)
-                evaluation_cost.append(cost)
-                print("Cost on evaluation data: {}".format(cost))
-            if monitor_evaluation_accuracy:
-                accuracy = self.accuracy(evaluation_data)
-                evaluation_accuracy.append(accuracy)
-                print("Accuracy on evaluation data: {} / {}".format(self.accuracy(evaluation_data), n_data))
-
-            # Early stopping:
-            if early_stopping_n > 0:
-                if accuracy > best_accuracy:
-                    best_accuracy = accuracy
-                    no_accuracy_change = 0
-                    #print("Early-stopping: Best so far {}".format(best_accuracy))
-                else:
-                    no_accuracy_change += 1
-
-                if (no_accuracy_change == early_stopping_n):
-                    #print("Early-stopping: No accuracy change in last epochs: {}".format(early_stopping_n))
-                    return evaluation_cost, evaluation_accuracy, training_cost, training_accuracy
-
-        return evaluation_cost, evaluation_accuracy, \
-            training_cost, training_accuracy
 
     def update_mini_batch(self, mini_batch, eta, lmbda, n):
         """Update the network's weights and biases by applying gradient
@@ -247,38 +195,21 @@ class Network(object):
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
         return (nabla_b, nabla_w)
 
-    def accuracy(self, data, convert=False):
+    def evaluate(self, data):
         """Return the number of inputs in ``data`` for which the neural
         network outputs the correct result. The neural network's
         output is assumed to be the index of whichever neuron in the
         final layer has the highest activation.
-
-        The flag ``convert`` should be set to False if the data set is
-        validation or test data (the usual case), and to True if the
-        data set is the training data. The need for this flag arises
-        due to differences in the way the results ``y`` are
-        represented in the different data sets.  In particular, it
-        flags whether we need to convert between the different
-        representations.  It may seem strange to use different
-        representations for the different data sets.  Why not use the
-        same representation for all three data sets?  It's done for
-        efficiency reasons -- the program usually evaluates the cost
-        on the training data and the accuracy on other data sets.
-        These are different types of computations, and using different
-        representations speeds things up.  More details on the
-        representations can be found in
-        mnist_loader.load_data_wrapper.
-
         """
-        if convert:
-            results = [(np.argmax(self.feedforward(x)), np.argmax(y))
-                       for (x, y) in data]
-        else:
-            results = [(np.argmax(self.feedforward(x)), y)
-                        for (x, y) in data]
+        results = [(np.argmax(self.feedforward(x)), 
+                    np.argmax(y)) for (x, y) in data]
 
-        result_accuracy = sum(int(x == y) for (x, y) in results)
-        return result_accuracy
+        total_correct = sum(int(x == y) for (x, y) in results)
+        return total_correct
+
+    def predict(self, data):
+        results = [np.argmax(self.feedforward(x)) for x in data]
+        return results
 
     def total_cost(self, data, lmbda, convert=False):
         """Return the total cost for the data set ``data``.  The flag
